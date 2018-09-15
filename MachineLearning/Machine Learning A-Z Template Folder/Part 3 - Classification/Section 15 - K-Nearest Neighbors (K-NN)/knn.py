fdfd6dfb187d94c74c76869efac286b98779ac5a
Delton Myalil Antony <36544374+deltonmyalil@users.noreply.github.com>
2018-09-15 16:24:11 +0530

    k-nearest neighbors done
    Predicted whether a customer is gonna buy an insanely expensive car

--- MachineLearning/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.py
+++ MachineLearning/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.py
@@ -1,36 +1,38 @@
-## My Classification Template
+# K-Nearest Neighbors (K-NN)
 
-# importing libraries
+# Importing the libraries
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# importing the dataset
-dataset = pd.read_csv("Filename.csv")
-X = dataset.iloc[:, [2,3]].values
-y = dataset.iloc[:,4].values
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 
-# data partitioning
-from sklearn.model_selection import train_test_split # sklearn.cross_validation deprecated, now use sklearn.model_selection
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# feature scaling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test) # crap, I forgot to scale the test set
-# or could'nt I have partitioned the data after feature scaling??
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-# fitting logistic regression to the training set
+# Fitting K-NN to the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
 
-# predicting the test set results
-y_pred = classifier.predict(X_test) # make instanc eof the classifier class named classifier
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
-# creating confusion matrix
+# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cMatrix = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
 
-# visualizing results for training set
+# Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -42,13 +44,13 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
+plt.title('K-NN (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# visualizing results for test set
+# Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -60,7 +62,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
+plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
